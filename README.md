a0761c5beb924875c820ae3c76c914b2485b9979
unknown <ginthozanv@gmail.com>
2023-02-21 20:46:13 +0530

    add readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Cafe Employee Manager - Frontend
-This project was build with <br /> 
+# Pearsonkelly Assignment - Frontend
+
+This project was build with <br />
 **ReactJS | Redux | AG Grid | Fromik**
 
 ## To run the project
@@ -9,11 +10,15 @@ This project was build with <br />
 • `npm start`
 
 ## Preview
+
 <!-- Preview live on [Netlify](https://calculate-everything.netlify.app/) <br />
 Backend live on [Heroku](https://cafe-employee.herokuapp.com/) -->
+
 Backend Repository: [https://github.com/ginthozan-v/cafe-employee-backend.git]
 
 ## My assumption from the requirement
-• **No same employee can work in 2 cafes:**  If a userId is assigned to cafe that user can't be assigned to another cafe. <br/>
+
+• **No same employee can work in 2 cafes:** If a userId is assigned to cafe that user can't be assigned to another cafe. <br/>
 • **Calculate number of days worked:** The day a user is assigned to a cafe is the Start date.
+
 ###### The DB is up on Mongodb cloud.
--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,24 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = 'http://localhost:5000';
+const baseUrl = "http://localhost:5000/api";
 const cafesUrl = `${baseUrl}/cafes`;
 const employeesUrl = `${baseUrl}/employees`;
 
 export const fetchCafes = () => axios.get(cafesUrl);
-export const fetchCafesBySearch = (searchQuery) => axios.get(`${cafesUrl}?location=${searchQuery.search}`);
+export const fetchCafesBySearch = (searchQuery) =>
+  axios.get(`${cafesUrl}?location=${searchQuery.search}`);
 export const createCafe = (newCafe) => axios.post(cafesUrl, newCafe);
-export const updateCafe = (id, updatedCafe) => axios.patch(`${ cafesUrl }/${ id }`, updatedCafe);
-export const deleteCafe = (id) => axios.delete(`${ cafesUrl }/${ id }`);
-export const addEmployeeToCafe = (id, addEmployee) => axios.patch(`${ cafesUrl }/${ id }/add-employee`, addEmployee);
+export const updateCafe = (id, updatedCafe) =>
+  axios.patch(`${cafesUrl}/${id}`, updatedCafe);
+export const deleteCafe = (id) => axios.delete(`${cafesUrl}/${id}`);
+export const addEmployeeToCafe = (id, addEmployee) =>
+  axios.patch(`${cafesUrl}/${id}/add-employee`, addEmployee);
 
 export const fetchEmployees = () => axios.get(employeesUrl);
-export const fetchEmployeesBySearch = (searchQuery) => axios.get(`${employeesUrl}?cafe=${searchQuery.search}`);
-export const createEmployee = (newEmployee) => axios.post(employeesUrl, newEmployee);
-export const updateEmployee = (id, updatedEmployee) => axios.patch(`${ employeesUrl }/${ id }`, updatedEmployee);
-export const deleteEmployee = (id) => axios.delete(`${ employeesUrl }/${ id }`);
+export const fetchEmployeesBySearch = (searchQuery) =>
+  axios.get(`${employeesUrl}?cafe=${searchQuery.search}`);
+export const createEmployee = (newEmployee) =>
+  axios.post(employeesUrl, newEmployee);
+export const updateEmployee = (id, updatedEmployee) =>
+  axios.patch(`${employeesUrl}/${id}`, updatedEmployee);
+export const deleteEmployee = (id) => axios.delete(`${employeesUrl}/${id}`);
